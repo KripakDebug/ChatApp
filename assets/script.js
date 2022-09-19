6050a39718407663b298aeb6927363505e0d40e1
KripakDebug <kastrula123@gmail.com>
2022-09-19 05:07:14 +0300

    chat

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+const socket = io();
+
+
+const modalReg = document.querySelector('.modal');
+const messageList = document.querySelector('.message-list');
+const formRegistration = document.querySelector('.form');
+const inputGetName = document.querySelector('#inputAddName');
+const listUsers = document.querySelector('.users');
+const formMessageSend = document.querySelector('.form-mesage');
+const inputSendMessage = document.querySelector('.message-input');
+
+
+
+function formListener() {
+    formRegistration.addEventListener('submit', (e) => {
+        e.preventDefault()
+        if(inputGetName.value !== ''){
+            socket.emit('name', inputGetName.value)
+            modalReg.style.display = 'none';
+        }
+        
+    })
+    
+    formMessageSend.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if(inputSendMessage.value !== ''){
+            socket.emit('chat message', {message: inputSendMessage.value, name: inputGetName.value })
+            inputSendMessage.value = '';
+        }
+    })
+    socket.on('get users', (data) => {
+        listUsers.innerHTML = '';
+        data.forEach(element => {
+            listUsers.innerHTML += `<li>${element}</li>`;
+        });
+        
+    })
+    socket.on('chat message', (data) => {
+        let date = new Date();
+        let output = String(date.getDate()).padStart(2, '0') + '/' + String(date.getMonth() + 1).padStart(2, '0') + '   ' + String(date.toLocaleTimeString().slice(0,-3));
+        messageList.innerHTML += `<div class='message-wrap'><li><span>${data.name}</span> ${data.message}</li> <strong>${output}</strong></div>`;
+        messageList.scrollTo(0, messageList.scrollHeight);
+        
+    })
+}
+
+
+
+formListener()
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+let users = [];
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.use(express.static(__dirname + '/assets'))
+
+io.on('connection', (socket) => {
+    function updateUserName() {
+        io.sockets.emit('get users', users);
+    }
+    socket.on('disconnect', (data) => {
+        users.splice(users.indexOf(socket.username), 1);
+       updateUserName();
+    })
+
+
+
+
+    socket.on('name', (data) => {
+        socket.username = data;
+        users.push(socket.username);
+        updateUserName();
+    })
+
+    socket.on('chat message', (data) => {
+        io.emit('chat message', {
+            message: data.message,
+            name: data.name
+        })
+
+    })
+})
+
+
+http.listen(3000, () => {
+    console.log('сервер старт');
+})
